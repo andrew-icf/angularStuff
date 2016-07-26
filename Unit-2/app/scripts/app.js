6448f61b0cf4a15063cdf92660b42e690f70c332
Andrew Vandermeer <innerchaosfitness@yahoo.com>
2016-07-26 11:36:54 -0600

    Finishing 04-custom-filters

--- Unit-2/app/scripts/app.js
+++ Unit-2/app/scripts/app.js
@@ -1,5 +1,5 @@
 var app = angular.module("myApp", []);
 
 app.controller("FirstController", function($rootScope, $scope){
-
+  $scope.name = "Andrew";
 });
--- Unit-2/app/scripts/filters.js
+++ Unit-2/app/scripts/filters.js
@@ -1,17 +1,37 @@
 app.filter('kebab', function(){
   return function(input){
-    input.replace(/_[0-9]/g , "-");
+    input.replace(/(_[0-9])/g , "-");
+    return input;
   };
 });
 
 app.filter('camel', function(){
   return function(input) {
-    input.replace(/_|-/g, "");
+    input.replace(/[^\w\s]/gi, '');
+    return input;
   };
 });
 
 app.filter('pigLatin', function(){
-  return function(input){
-  input.replace();  
+    function letters(word) {
+    return word.split('');
+    }
+
+    function pigLatinizeWord(word) {
+        var chars = letters(word);
+        return chars.slice(1).join('') + chars[0] + 'ay';
+    }
+
+    function pigLatinizeSentence(sentence) {
+        return sentence.replace(/\w+/g, pigLatinizeWord);
+    }
+    return sentence;
+  });
+
+
+app.filter('redact', function(){
+  return function(input, word){
+    var reg = new RegExp(word, "g");
+    return input.replace(reg ,"REDACTED");
   };
 });
